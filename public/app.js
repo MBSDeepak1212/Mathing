b0321f5a19258c3276ee06fb025008085e82bc68
deepak-939215 <mbsdeepak2@gmail.com>
2023-02-25 20:30:44 +0530

    Functioning Calculator

--- public/app.js
+++ public/app.js
@@ -1,1 +1,18 @@
-console.log('Hello, world!');
+const button = document.getElementById("touch");
+const del = document.getElementById("DE");
+const alc = document.getElementById("AC");
+const disp = document.getElementById("disp");
+
+button.addEventListener("click", function() {
+  const name = eval(disp.value);
+  disp.value = name.toString();  
+});
+
+alc.addEventListener("click", function(){
+  disp.value = "";
+});
+
+del.addEventListener("click", function(){
+  const name = disp.value;
+  disp.value = name.slice(0,-1);
+});
